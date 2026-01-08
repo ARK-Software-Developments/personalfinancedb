207b0c13aded28017205990ec6d75ec576f723de
Andres Kamycki <andres.kamycki@gmail.com>
2026-01-08 17:47:32 -0300

    akamycki/61

--- personalfinance_routines.sql
+++ personalfinance_routines.sql
@@ -832,6 +832,48 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `spCreditCardSpendingtGetResumenByCard` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spCreditCardSpendingtGetResumenByCard`(
+	IN pYear INT,
+    IN pCardsId INT
+)
+BEGIN
+	SELECT 
+    `purchasingentity`, 
+    SUM(`january`) AS january,
+    SUM(`february`) AS february,
+    SUM(`march`) AS march,
+    SUM(`april`) AS april,
+    SUM(`may`) AS may,
+    SUM(`june`) AS june,
+    SUM(`july`) AS july,
+    SUM(`august`) AS august,
+    SUM(`september`) AS september,
+    SUM(`october`) AS october,
+    SUM(`november`) AS november,
+    SUM(`december`) AS december
+FROM
+    `creditcardspending`
+WHERE
+    `year` = pYear
+    AND `cardsid` = pCardsId
+GROUP BY 
+    `purchasingentity`;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `spCreditCardSpendingtUpdate` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -2203,4 +2245,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-01-08  0:29:56
+-- Dump completed on 2026-01-08 17:44:27
