d6c451196bcf6671415a97ce48b5d359375882c6
Andres Kamycki <andres.kamycki@gmail.com>
2026-01-09 12:58:10 -0300

    akamycki/69: Agregar funcionalidad para copiar ingresos del mes a los siguientes

--- personalfinance_routines.sql
+++ personalfinance_routines.sql
@@ -1072,6 +1072,87 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `spIncomeCopyBudget` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spIncomeCopyBudget`(
+	IN pYearFrom INT,
+    IN pYearTo INT,
+    IN pMonthFrom INT,
+    IN pMonthTo INT
+)
+BEGIN
+	DECLARE done INT DEFAULT 0;
+    DECLARE v_incomedetailsid INT;
+    DECLARE v_selected_month DECIMAL(10,2);
+    
+    DECLARE cur CURSOR FOR
+        SELECT `incomedetailsid`, 
+        CASE
+			WHEN pMonthFrom = 1 THEN `january`
+			WHEN pMonthFrom = 2 THEN `february`
+			WHEN pMonthFrom = 3 THEN `march`
+            WHEN pMonthFrom = 4 THEN `april`
+            WHEN pMonthFrom = 5 THEN `may`
+            WHEN pMonthFrom = 6 THEN `june`
+            WHEN pMonthFrom = 7 THEN `july`
+            WHEN pMonthFrom = 8 THEN `august`
+            WHEN pMonthFrom = 9 THEN `september`
+            WHEN pMonthFrom = 10 THEN `october`
+            WHEN pMonthFrom = 11 THEN `november`
+            WHEN pMonthFrom = 12 THEN `december`
+		END AS selected_month
+        FROM `income`
+        WHERE `year` = pYearFrom;
+
+    -- Handler para fin del cursor
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+
+    -- Abrir el cursor
+    OPEN cur;
+
+    read_loop: LOOP
+        FETCH cur INTO v_incomedetailsid, v_selected_month;
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
+
+        -- Aquí puedes hacer lo que necesites con los valores
+		-- Actualizar el año destino
+		UPDATE `income`
+		SET 
+			`january`   = CASE WHEN pMonthTo = 1  THEN v_selected_month ELSE `january` END,
+			`february`  = CASE WHEN pMonthTo = 2  THEN v_selected_month ELSE `february` END,
+			`march`     = CASE WHEN pMonthTo = 3  THEN v_selected_month ELSE `march` END,
+			`april`     = CASE WHEN pMonthTo = 4  THEN v_selected_month ELSE `april` END,
+			`may`       = CASE WHEN pMonthTo = 5  THEN v_selected_month ELSE `may` END,
+			`june`      = CASE WHEN pMonthTo = 6  THEN v_selected_month ELSE `june` END,
+			`july`      = CASE WHEN pMonthTo = 7  THEN v_selected_month ELSE `july` END,
+			`august`    = CASE WHEN pMonthTo = 8  THEN v_selected_month ELSE `august` END,
+			`september` = CASE WHEN pMonthTo = 9  THEN v_selected_month ELSE `september` END,
+			`october`   = CASE WHEN pMonthTo = 10 THEN v_selected_month ELSE `october` END,
+			`november`  = CASE WHEN pMonthTo = 11 THEN v_selected_month ELSE `november` END,
+			`december`  = CASE WHEN pMonthTo = 12 THEN v_selected_month ELSE `december` END
+		WHERE `year` = pYearTo 
+		  AND `incomedetailsid` = v_incomedetailsid;
+
+    END LOOP;
+
+    -- Cerrar el cursor
+    CLOSE cur;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `spIncomeDetailsAdd` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -2245,4 +2326,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-01-08 17:44:27
+-- Dump completed on 2026-01-09 12:56:20
